96f009077de4467a3003d3a72fc12529bc2bb2e9
Maria Eduarda Sales de Castro <123908787+MariaCastr0@users.noreply.github.com>
2023-11-21 09:24:52 -0300

    Update consultar_chamado.php

--- consultar_chamado.php
+++ consultar_chamado.php
@@ -8,7 +8,7 @@
   //abrir o arquivo.hd
   $arquivo = fopen('../../app_help_desk/arquivo.hd', 'r');
 
-  //enquanto houver registros (linhas) a serem recuperado
+  //enquanto houver registros (linhas) a serem recuperados
   while(!feof($arquivo)) { //testa pelo fim de um arquivo
     //linhas  
     $registro = fgets($arquivo);
